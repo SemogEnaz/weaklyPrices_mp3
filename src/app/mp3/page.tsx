45401f53749bc17179eb68bd660a8bab03b69d3a
Zane Gomes <semogenaz@gmail.com>
2024-01-02 23:27:20 +1100

    Added placeholder for yt-dlp project

---
+++ src/app/mp3/page.tsx
@@ -0,0 +1,11 @@
+import { HomeButton } from '@/ui/button'
+
+export default function Page() {
+    return (
+        <div className='h-[100vh] flex flex-col items-center justify-around'>
+            <HomeButton />
+            <p>Place holder for project :)</p>
+        </div>
+    )
+    
+}
