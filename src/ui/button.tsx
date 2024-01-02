3e57dad1638bd0d2ce675128a993bb69a9eeeae1
Zane Gomes <semogenaz@gmail.com>
2024-01-02 23:24:49 +1100

    Added navigation buttons as components

---
+++ src/ui/button.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+
+import '@/ui/button.css'
+
+export function TopButton({ link, text }) {
+    return (
+        <Link 
+            href={link}
+            className='button-top'
+        >
+          {text}
+        </Link>
+    )
+}
+
+export function Button({ link, text }) {
+    return (
+        <Link 
+            href={link}
+            className='button'
+        >
+          {text}
+        </Link>
+    )
+}
+
+export function HomeButton() {
+    return (
+        <Button link={'/'} text={"Home"} />
+    );
+}
